53e01dd670065081991c165a6dc809ebe1eb3086
myungsworld <myungsworld@gmail.com>
2022-08-18 14:35:47 +0900

    add prompt to make component reuse

--- egoing/src/App.js
+++ egoing/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Subject(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      {props.sub}
+    </header>
+  );
+}
+
+function TOC() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+      </ul>
+    </nav> 
+  );
+}
+
+function Content(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.desc}
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Subject title="React" sub="world wide web!"></Subject>
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
   );
 }
