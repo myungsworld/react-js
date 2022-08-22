7e1808cb9538b63b8c7ad88bf9b9b6e9c18ebde4
myungsworld <myungsworld@gmail.com>
2022-08-22 13:22:21 +0900

    return toc arrays

--- egoing/src/App.js
+++ egoing/src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
+import "./App.css";
 import TOC from "./components/TOC";
-import Content from "./components/Content"
-import Subject from "./components/Subject"
-
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
 function App() {
   const topics = [
-    {id : 1 , title:"Song"},
-    {id : 2 , title : "Dong"},
-    {id : 3 , title :"Myung"}
-  ]
+    { id: 1, title: "Song" },
+    { id: 2, title: "Dong" },
+    { id: 3, title: "Myung" },
+  ];
   return (
-    <div className='App'>
+    <div className="App">
       <Subject title="Web" sub="world wide web!"></Subject>
       <TOC topics={topics}></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
--- egoing/src/components/TOC.js
+++ egoing/src/components/TOC.js
@@ -1,16 +1,18 @@
 function TOC(props) {
-  const lis = []
-  const topics = props.topics
+  const topics = props.topics;
   const listItems = topics.map((topic) => {
-    lis.push(<li key={topic.id}><a href={"/"+topic.id}>{topic.title}</a></li>)
-  })
-  return (
-      <nav>
-        <ul>
-          {lis}
-        </ul>
-      </nav> 
+    return (
+      <li key={topic.id}>
+        <a href={"/" + topic.id}>{topic.title}</a>
+      </li>
     );
-  }
+  });
 
-export default TOC;  
+  return (
+    <nav>
+      <ul>{listItems}</ul>
+    </nav>
+  );
+}
+
+export default TOC;
